d0f3f6ed0b5ee9cd37dcab58a8e327a270003a1b
Dave Ames <david.john.ames@gmail.com>
2024-09-11 08:26:04 +0100

    Initial project setup

---
+++ src/test/java/com/booleanuk/battleships/BattleshipsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booleanuk.battleships;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleshipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
